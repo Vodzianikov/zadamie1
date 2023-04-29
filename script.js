1b375a01b47996fb434e5c3eece1e665ae4cd34c
Vodzianikov <125147632+Vodzianikov@users.noreply.github.com>
2023-04-29 22:08:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('Введите 2 число, что бы найти большее');
+let numb1 = prompt('Введите первое число');
+let numb2 = prompt('Введите второе число');
+if (numb1 > numb2){
+    console.log(`numb1`)
+}
+if (numb1 < numb2){
+    console.log(`numb2`)
+}
